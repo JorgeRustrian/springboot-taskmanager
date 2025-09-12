f9cb831b1fe653c10511f65516d06406df78a5f2
JorgeRustrian <estuardojorge820@gmail.com>
2025-09-12 11:53:14 -0600

    Inicializar proyecto con Spring Boot

---
+++ src/test/java/com/proyectoCRUD/crudTareas/CrudTreasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoCRUD.crudTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTreasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
